a46f1ad27a15e65d423c1950b7de6498a72b3ff2
Gonef <64199703+Gonef@users.noreply.github.com>
2020-06-10 14:47:35 +0200

    1.2. Final
    Changed KPScript to KeePass powershell module.
    Added checking if keepass entries and folders exist and adding them if don't
    Fixed error with regex special characters

--- ResetPassword.ps1
+++ ResetPassword.ps1
@@ -1,5 +1,5 @@
 #For user: Define Password Properties: Excluded chars, password length
-$pwdExcludeCharsString = '<>;'
+#$pwdExcludeCharsString = '<>()&?#$%'
 $pwdLength = 20
 $pwdMinCapitalCount = 2
 $pwdMinSmallCount = 2
@@ -12,29 +12,33 @@ $pwdSmall = 'abcdefghiklmnoprstuvwxyz'
 $pwdSpecial = '!"§$%/()=?}][{@#*+<>;'
 $pwdNumbers = '1234567890'
 
+#$pwdExcludeChars = $pwdExcludeCharsString.ToCharArray()
+
+#Import Modules
+Import-Module C:\Modules\CsvInputModule.psm1
+Import-Module C:\Modules\KeePassModule.psm1
+
+$ScriptUser = whoami
+$timestamp = get-date
+$inputfile = Get-FileName ".\" 
+$csv = Import-Csv $inputfile -Delimiter ";"
+$KeePassDB = 'c:\keepassdb\lab.kdbx'
+$KeePassBin = 'C:\Apps\KeePass'
+$logFile = 'c:\Scripts\ResetPassword\logs.txt'
+$errorFile = 'c:\Scripts\ResetPassword\errors.txt'
+
+#Get KeePass Password from user
+$KeePassPwd = Read-Host -AsSecureString -Prompt "Enter Master Keepass Password"
+$DB = Open-Keepassdatabase -SecuredPasswordToDB $KeePassPwd -PathToDB $KeePassDB -PathToKeePassBinFolder $KeePassBin
+
+#Get Excluded Characters from user
+$pwdExcludeCharsString = Read-Host -Prompt "Enter characters you dont want to use in password. Leave blank if there is no forbidden characters"
 $pwdExcludeChars = $pwdExcludeCharsString.ToCharArray()
 
-#input group name, share path and domain from csv file
-Function Get-FileName($directory){
-    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
-    
-    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
-    $OpenFileDialog.initialDirectory = $directory
-    $OpenFileDialog.filter = "CSV (*.csv)| *.csv"
-    $OpenFileDialog.ShowDialog() | Out-Null
-    $OpenFileDialog.filename
-    
+foreach ($Char in $pwdExcludeChars){
+    $charTemp =  '\' + $Char
+    $pwdSpecial = $pwdSpecial -replace $charTemp, ''
     }
-
-$timestamp = get-date
-
-$inputfile = Get-FileName ".\" 
-
-$csv = Import-Csv $inputfile -Delimiter ";" 
-
-#Get KeePass Password from user than convert it to plain text so cmd can use it.
-$KeePassPwdEncrypted = Read-Host -AsSecureString -Prompt "Enter Master Keepass Password"
-$KeePassPwd = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($KeePassPwdEncrypted))
 
 #Create Function for getting random chars from list
 function GetRandomChars($length, $characters){
@@ -51,11 +55,6 @@ function ChangeOrder([string]$inputString){
     $RandomizedStringArray = $charArray | Get-Random -Count $charArray.Length
     $outputString = -join $RandomizedStringArray
     return $outputString
-    }
-
-foreach ($Char in $pwdExcludeChars){
-    $charTemp =  '\' + $Char
-    $pwdSpecial = $pwdSpecial -replace $Char, ''
     }
 
 #Merge the rest of chars to make it more random
@@ -77,9 +76,52 @@ function CreatePwd{
 #Reset password and update KeePass entries
 foreach ($line in $csv){
     $user = $line.user
+    $domain = $line.domain
+    $domainusername = $user+'@'+$domain
     $pwd = CreatePwd
-    Set-ADAccountPassword -Server $line.domain -Identity $line.user -Reset -NewPassword (ConvertTo-SecureString -AsPlainText "$pwd" -Force)
-    &"C:\Apps\KeePass\KPScript.exe" -c:EditEntry "C:\KeePassDB\Lab.kdbx" -pw:$KeePassPwd -ref-Title:$user -set-Password:$pwd
-    Write-Host "Password changed for user" $line.user ". New password:" $pwd
+    $pwdEncrypted = ConvertTo-SecureString $pwd -AsPlainText -Force
+    try {
+        $userInfo = Get-ADUser -Identity $user -Server $domain
+        $entry = Get-KeePassEntry -UserName $user -KeePassDB $DB
+        Set-ADAccountPassword -Server $domain -Identity $user -Reset -NewPassword $pwdEncrypted
+        
+        # if keepass entry exist
+        if($entry.count -eq 1){
+            Update-KeePassEntry -UserName $domainusername -KeePassDB $DB -newPassword $pwdEncrypted
+            Write-Host "Password changed for user $user . New password: $pwd `r`nKeePass Entry updated" -foregroundcolor green
+            Write-Output "$timestamp $ScriptUser changed password for $user" >> $logFile
+            }
+
+        # if keepass entry does not exist
+        elseif($entry.count -eq 0){
+            $userOU = ($userInfo.DistinguishedName -split "OU=",3)[2]
+            $userApp = $userOU.Substring(0,$userou.IndexOf(','))
+            $groupsExist = Check-ifKeePassFolderExists -GroupName $userApp -PrimaryGroup "Applications" -KeePassDB $DB
+            
+            #if group doesnt exist in keepass
+            if(!$GroupsExist){
+                Add-KeepassGroup -GroupName $userApp -KeePassDB $DB -PrimaryGroup "Applications"
+                }
+            Add-KeepassEntry -GroupName $userApp -PrimaryGroup "Applications" -KeePassDB $DB -Title $domainusername -Username $user -Password $pwdEncrypted
+            # KeePAssModule use write-host to let know that he added new folder and entry
+            Write-Host "Password changed for user $user . New password: $pwd" -foregroundcolor green
+            Write-Output "$timestamp $ScriptUser changed password for $user" >> $logFile
+            }
+
+        # if there is more than 1 keepas entry
+        else{
+             Write-Host "Password changed for user $user . New password: $pwd" -foregroundcolor green
+             Write-Output "$timestamp $ScriptUser changed password for $user" >> $logFile
+             Write-Host "KeePass Entry has not been updated, please check it manually. There is probably more than 1 entry of that title" -foregroundcolor red
+            }
+            
+        }
+    Catch {
+        Write-Host "Password wasn't changed for user $user . User doesn't exist or you don't have access to edit this account" -foregroundcolor red
+        Write-Output "$timestamp Password wasn't changed for user $user . User doesn't exist or $ScriptUser doesn't have access to edit this account" >> $errorFile     
+        }
     }
-Write-Host "KeePass entries have been updated"
+
+$db.close()
+$KeePassPwd = ""
+$Pwd = ""
