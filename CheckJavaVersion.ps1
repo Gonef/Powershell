5f47a7ce1a4971a514303cbc1fe4fcd8a1d13da0
Gonef <64199703+Gonef@users.noreply.github.com>
2021-04-20 01:32:31 +0200

    Create CheckJavaVersion.ps1

---
+++ CheckJavaVersion.ps1
@@ -0,0 +1,56 @@
+
+Function Get-FileName($directory){
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+    
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $OpenFileDialog.initialDirectory = $directory
+    $OpenFileDialog.filter = "CSV (*.csv)| *.csv"
+    $OpenFileDialog.ShowDialog() | Out-Null
+    $OpenFileDialog.filename
+    
+    }
+
+$inputfile = Get-FileName ".\" 
+$TimeStamp = Get-Date -Format "MM-dd-yyyy-HH-mm"
+$csv = Import-Csv $inputfile -Delimiter ";" 
+$JavaVersion = Invoke-Command -cn $srv {get-package java*} | Select-Object Name,Source
+$JavaTable = @()
+$ErrorLog = @()
+
+foreach ($line in $csv)
+    {
+
+    $srv = $line.server + '.' + $line.domain
+        if (Test-Connection -Computername $srv -Quiet -Count 1)
+        {
+            Write-Host "Processing server: $srv "
+            try
+            {
+            Enable-WSManCredSSP -Force -Role "Client" -DelegateComputer $srv
+            Invoke-Command -cn $srv { Enable-WSManCredSSP -Force -Role "Client" -DelegateComputer PDC }
+            Invoke-Command -cn $srv { Set-Item WSMan:\localhost\Service\Auth\CredSSP true }
+
+            foreach ($Row in $JavaVersion)
+                {
+                $JavaTableBuild = [ordered]@{
+                    ServerName = $line.server
+                    Version = $Row.Name
+                    Localization = $Row.Source
+                    }
+                    $JavaTable += New-Object PSObject -Property $JavaTableBuild
+                }
+            }
+            catch
+            {
+            $ErrorLog += "$timestamp Unable to Check Java Version on $srv"
+            }
+        }
+        else
+        {
+        $ErrorLog += "$timestamp Unable to Check Java Version on $srv"
+        }
+    }
+
+$JavaTable | Export-Csv -Path .\"$TimeStamp.csv" -NoTypeInformation
+Write-Host "Check Error.log file to see if any errors occured." -ForegroundColor Red
+$ErrorLog >> errors.log
