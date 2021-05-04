9a3016d20001972b63f52f39f11df92041900054
Gonef <64199703+Gonef@users.noreply.github.com>
2021-05-04 13:50:59 +0200

    Create ShareInfo.ps1

---
+++ ShareInfo.ps1
@@ -0,0 +1,40 @@
+Function Get-FileName($directory)
+{
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+    
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $OpenFileDialog.initialDirectory = $directory
+    $OpenFileDialog.filter = "CSV (*.csv)| *.csv"
+    $OpenFileDialog.ShowDialog() | Out-Null
+    $OpenFileDialog.filename
+    
+}
+
+$inputfile = Get-FileName ".\" 
+
+$TimeStamp = Get-Date -Format "MM-dd-yyyy-HH-mm"
+
+$csv = Import-Csv $inputfile -Delimiter ";"
+
+foreach ($line in $csv)
+    {
+    $ServerName = $line.ServerName    
+    $ShareList = Invoke-Command -cn $ServerName {Get-SmbShare}
+    $AccessList = @()
+
+    foreach ($row in $ShareList)
+        {
+            try
+            {
+            $AccessList += Get-Acl -path $row.Path
+            }
+            catch
+            {
+            continue
+            }
+
+        }
+    
+    $AccessList | Export-csv -Path .\$ServerName.csv -NoTypeInformation -Delimiter ";"
+
+    }
